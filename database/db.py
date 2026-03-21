e56fda3fa526bd561bbdc4adeb38c187a04120a4
Florian Scherer <florian.scherer@gmx.at>
2026-03-21 12:05:21 -0400

    inital commit. created simple scraper for one specific data source website.

---
+++ database/db.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv("database/.env")
+
+
+def get_connection():
+    """
+    Establishes and returns a connection to the PostgreSQL database using environment variables.
+
+    :return: A psycopg2 connection object.
+    """
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
+    )
